b5b269ed263247cac989277053c096abf25d30e9
awais khan <awaisalishah02@gmail.com>
2025-06-05 02:21:03 +0500

    POSTGRESSQL CONFIG

--- FoodOnline_main/settings.py
+++ FoodOnline_main/settings.py
@@ -74,8 +74,11 @@ WSGI_APPLICATION = 'FoodOnline_main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'FoodOnline_DB',
+        "USER":"postgres",
+        "PASSWORD":"djangoadmin",
+        "PORT":"5432"
     }
 }
 
