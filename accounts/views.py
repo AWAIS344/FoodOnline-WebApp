e069a82458890374f2c297a7b76c55155a4ca52f
awais khan <awaisalishah02@gmail.com>
2025-06-06 22:43:28 +0500

    Registration Page Add

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def RegisterUser(request):
+    return HttpResponse("This is User Registration")
