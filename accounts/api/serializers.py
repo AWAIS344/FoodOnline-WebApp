1239f6cbc393b6a4487bd567aef58d7a2a4c2e29
awais khan <awaisalishah02@gmail.com>
2025-06-10 20:35:47 +0500

    User API added

---
+++ accounts/api/serializers.py
@@ -0,0 +1,33 @@
+# serializers.py
+
+from rest_framework import serializers
+from accounts.models import User
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'first_name', 'last_name', 'username', 'password',
+            'confirm_password', 'phone_number', 'email'
+        ]
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.role = User.CUSTOMER
+        user.save()
+        return user
+2
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .api.views import RegisterUserAPI
 
 urlpatterns = [
-    path('registeruser/',views.RegisterUser,name="register")
+    path('registeruser/',views.RegisterUser,name="register"),
+    path('api/register/', RegisterUserAPI.as_view(), name='user-register'),
 ]
