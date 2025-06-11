ca20ae6f2fd8addee8435dfa7a4352b033b58382
awais khan <awaisalishah02@gmail.com>
2025-06-11 21:35:12 +0500

    Vendor Models Added

--- FoodOnline_main/settings.py
+++ FoodOnline_main/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from django.contrib.messages import constants as message_constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     "accounts",
+    "vendor",
 ]
 
 MIDDLEWARE = [
@@ -136,3 +138,15 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+MESSAGE_TAGS = {
+    message_constants.DEBUG: 'debug',
+    message_constants.INFO: 'info',
+    message_constants.SUCCESS: 'success',
+    message_constants.WARNING: 'warning',
+    message_constants.ERROR: 'danger',
+}
+
--- FoodOnline_main/urls.py
+++ FoodOnline_main/urls.py
@@ -23,5 +23,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,name="home"),
-    path('account/', include("accounts.urls"))
+    path('account/', include("accounts.urls")),
+    # path('vendor/', include("vendor.urls"))
+
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
