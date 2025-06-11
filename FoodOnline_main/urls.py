9d123d7f625dc9e3408011426dafae37896e30d6
awais khan <awaisalishah02@gmail.com>
2025-06-12 00:23:08 +0500

    Vendor urls and view config

--- FoodOnline_main/urls.py
+++ FoodOnline_main/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,name="home"),
     path('account/', include("accounts.urls")),
-    # path('vendor/', include("vendor.urls"))
+    path('vendor/', include("vendor.urls")),
 
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
---
+++ vendor/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("registervendor/",views.RegisterVendor,name="registervendor")
+]
