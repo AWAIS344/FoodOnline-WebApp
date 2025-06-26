68dde3fabc581d5e926aa53ed5b86f7cc26ab0de
awais khan <awaisalishah02@gmail.com>
2025-06-26 22:04:07 +0500

    Vendor Dashboard Extra Cards Added

---
+++ vendor/validator.py
@@ -0,0 +1,8 @@
+from django.core.exceptions import ValidationError
+import os
+
+def allow_only_image_validator(value):
+    ext=os.path.splitext(value.name)[1]
+    valid_ext=[".png",".jpg",",jpeg"]
+    if not ext.lower() in valid_ext:
+        raise ValidationError("Unsupported file - Allowed extensions :" + str(valid_ext))
