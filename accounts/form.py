a14f5b65586067f9c6f3e9fb26468c729fff51cc
awais khan <awaisalishah02@gmail.com>
2025-06-27 16:03:05 +0500

    Validation - Form Erros - making the form ready for using GOOGLE API

--- accounts/form.py
+++ accounts/form.py
@@ -1,5 +1,6 @@
 from .models import User,UserProfile
 from  django import forms
+from vendor.validator import allow_only_image_validator
 
 class UserRegForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={
@@ -25,11 +26,19 @@ class UserRegForm(forms.ModelForm):
         
 
 class UserProfileForm(forms.ModelForm):
-    profile_image=forms.ImageField(widget=forms.FileInput(attrs={"class":"btn btn-info"}))
-    cover_image=forms.ImageField(widget=forms.FileInput(attrs={"class":"btn btn-info"}))
+    profile_image=forms.FileField(widget=forms.FileInput(attrs={"class":"btn btn-info"}),validators=[allow_only_image_validator])
+    cover_image=forms.FileField(widget=forms.FileInput(attrs={"class":"btn btn-info"}),validators=[allow_only_image_validator])
     class Meta:
         model = UserProfile
         exclude = ["user", "created_at", "modified_at"]
+
+
+    #MAKING THE FIELDS READONLY
+    def _init__(self,*args, **kwargs):
+        super(UserProfileForm,self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == "latitude" or field == "longitude":
+                self.fields['field'].widget.attrs['readonly']='readonly'
 
 
     
