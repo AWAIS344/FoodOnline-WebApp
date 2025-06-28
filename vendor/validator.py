34d28c81f701ff69fc2336cf02866804863be61f
awais khan <awaisalishah02@gmail.com>
2025-06-28 21:48:40 +0500

    Validator Bug Removed

--- vendor/validator.py
+++ vendor/validator.py
@@ -3,6 +3,6 @@ import os
 
 def allow_only_image_validator(value):
     ext=os.path.splitext(value.name)[1]
-    valid_ext=[".png",".jpg",",jpeg"]
+    valid_ext=[".png",".jpg",".jpeg"]
     if not ext.lower() in valid_ext:
         raise ValidationError("Unsupported file - Allowed extensions :" + str(valid_ext))
