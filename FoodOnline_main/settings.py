b649f551adc247bd829e2671d697a8b07fb9772a
awais khan <awaisalishah02@gmail.com>
2025-07-01 18:47:53 +0500

    Google API KEY ADDED

--- FoodOnline_main/settings.py
+++ FoodOnline_main/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor',
+                "accounts.context_processors.get_google_api",
             ],
         },
     },
@@ -161,3 +162,7 @@ EMAIL_HOST_USER=config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD=config("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS=True
 DEFAULT_FROM_EMAIL = "FoodOnline MarketPlace <jamescharles877923@gmail.com >"
+
+GOOGLE_API_KEY="AIzaSyDnd9JwZvXty-1gHZihMoFhJtCXmHfeRQg"
+
+
