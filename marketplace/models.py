cecc510a551e6492a89ec86a7f6ae2883bf4fcda
awais khan <awaisalishah02@gmail.com>
2025-07-13 23:48:51 +0500

    Cart Model Added

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from accounts.models import User
+from menu.models import FoodItems
 
 # Create your models here.
+
+class Cart(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    footitem=models.ForeignKey(FoodItems,on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
